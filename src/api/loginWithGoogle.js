17322218993ec0d1be96f5194c96c9f9fe066cc2
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-30 10:04:11 +0300

    added loginWithGoogle route

---
+++ src/api/loginWithGoogle.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+router.get('/login/google', passport.authenticate('google', { scope: [ 'profile', 'email' ] }));
+
+const successLoginUrl = 'http://localhost:3000/login/success';
+const errorLoginUrl = 'http://localhost:3000/login/error';
+
+router.get(
+	'/auth/google/callback',
+	passport.authenticate('google', {
+		failureMessage: 'Cannot login to Google, please try again later!',
+		failureRedirect: errorLoginUrl,
+		successRedirect: successLoginUrl
+	}),
+	(req, res) => {
+		console.log('User =========', req.user);
+		res.send('Thank you for siging in!');
+	}
+);
+
+module.exports = router;
