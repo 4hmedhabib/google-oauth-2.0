037cbf4076d1c8550575595e1fef6693f57d9af9
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-30 10:28:56 +0300

    updated some files

--- src/auth/passportGoogleSSO.js
+++ src/auth/passportGoogleSSO.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/user');
-const callBackUrl = 'localhost:5000/api/v1/auth/google/callback';
+const callBackUrl = 'http://localhost:5000/api/v1/auth/google/callback';
 passport.use(
 	new GoogleStrategy(
 		{
@@ -11,17 +11,30 @@ passport.use(
 			passReqToCallback: true
 		},
 		async (req, accessToken, refreshToken, profile, cb) => {
-			const defaultUser = {
-				fullName: `${user.name.givenname} ${user.name.familyName}`,
+			const defaultUser = await {
+				fullName: `${profile.name.givenName} ${profile.name.familyName}`,
+				email: profile.emails[0].value,
 				picture: profile.photos[0].value,
 				googleId: profile.id
 			};
+
+			const isUser = await User.findOne({ where: { googleId: profile.id } })
+				.then((user) => {
+					if (!user) {
+						console.log('======================== User Does not Exists', profile.displayName);
+						return;
+					}
+					console.log('======================== User Is Exists', profile.displayName);
+				})
+				.catch((err) => {
+					cb(err, null);
+				});
 
 			const user = await User.findOrCreate({
 				where: { googleId: profile.id },
 				defaults: defaultUser
 			}).catch((err) => {
-				cosnole.log('Error signin up ================', err);
+				cosnole.log('Error signin in ================', err);
 				cb(err, null);
 			});
 
